ff73196bf981f68c72d7db0d5e4836214fee2275
profuentes2022 <100309580+profuentes2022@users.noreply.github.com>
2024-04-15 10:54:05 -0600

    docuemtoincio
    creacion de bak
    documento de inicio

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # proyectoprogra123
+
+fase1:
+crear un login que permita el registro de usuario y un admin
+
+dba: creat base de datos: "auditorias" 
+crear la tabla usuarios-> id autoincremet,nombre varchar(), correo varcahr(),rol varchar();
+tabla de vitacora par insert,delete,update.
+
+
+back: node js sevidor: localhost:4444
+creacion loandi page: 
+enpoint: localhost:4444/usuarios
+         metodo: post variable {nom,cor,rol}
+         metodo: get /usuarios loign
+         metodo: put /usuarios varibles {id,nombre,corre,rol}
+
+
+front: 
+    form:login
+    form:registros usarios
+    form:eliminaicon
+    form:actulizar datos
---
+++ back/index.js
@@ -0,0 +1,121 @@
+const express=require('express');
+const mysql=require('mysql');
+const bodyParser=require('body-parser');
+const cors=require('cors');
+
+const server=express();
+
+server.use(express.json());
+
+server.use(cors({
+    origin:'*',
+    methods:'HEAD,GET,PUT,DELETE,POST,PATHC',
+}));
+
+
+//configuracion base de datos 
+const configdb={
+    host:'localhost',
+    user:'root',
+    passoword:'',
+    database:'consultas'
+};
+
+const poolmysql=mysql.createPool(configdb);
+
+
+
+server.post("/usuarios",(req,rep)=>{
+    console.log(req);
+    let nombre=req.body.nombre;
+    let edad=req.body.edad;
+    let fecha=req.body.fecha;
+    const slq=`INSERT INTO datos (nombre, edad, fecha) VALUES ('${nombre}', '${edad}', '${fecha}')`;
+    console.log(slq);
+    poolmysql.query(slq,function(err,result){
+    if(err) throw  rep.status(401).send(201);
+    });
+    rep.status(201).send(201);
+    });
+    
+
+
+server.get("/produto",(req,resp)=>{
+    const slq="select * from datos";
+    poolmysql.query(slq,function(err,result){
+        if(err) throw resp.end("error en base de datos");
+        resp.json(result);
+    });
+});
+
+
+
+
+server.get("/datos",(req,resp)=>{
+    const slq="select * from datos";
+    poolmysql.query(slq,function(err,result){
+        if(err) throw resp.end("error en base de datos");
+        resp.json(result);
+    });
+});
+
+server.post("/datos",(req,rep)=>{
+console.log(req);
+let nombre=req.body.nombre;
+let edad=req.body.edad;
+let fecha=req.body.fecha;
+const slq=`INSERT INTO datos (nombre, edad, fecha) VALUES ('${nombre}', '${edad}', '${fecha}')`;
+poolmysql.query(slq,function(err,result){
+if(err) throw  rep.status(201).send(201);
+});
+rep.status(201).send(201);
+});
+
+
+
+
+server.get("/datos/:id",(req,rep)=>{
+let idc=req.params.id;
+const slq="select * from datos where id=" + idc;
+poolmysql.query(slq,function(err,result){
+    if(err) throw rep.end("error en base de datos");
+    rep.json(result);
+});
+
+
+
+
+});
+
+
+
+
+server.delete("/datos",(req,rep)=>{
+    let id=req.body.id;
+    const slq=`delete from datos where id='${id}'`;
+    poolmysql.query(slq,function(err,result){
+    if(err) throw  rep.status(201).send(201);
+    });
+    rep.status(201).send(201);
+    });
+    
+
+    server.put("/datos",(req,rep)=>{
+        let nombre=req.body.nombre;
+        let edad=req.body.edad;
+        let fecha=req.body.fecha;
+        let id=req.body.id;
+        const slq=`update datos set nombre='${nombre}', edad='${edad}', fecha='${fecha}' where id=${id}`;
+        poolmysql.query(slq,function(err,result){
+        if(err) throw  rep.status(201).send(201);
+        });
+        rep.status(201).send(201);
+        });
+
+
+
+
+
+server.listen(5555,()=>{
+    console.log('servidor en linea en el puerto 5555');
+});
